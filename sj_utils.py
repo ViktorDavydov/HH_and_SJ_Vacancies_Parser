138f3039cef06280c5545e68a3e2f909bc93c3be
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-10-03 23:34:51 +0300

    Готовая программа

--- sj_utils.py
+++ sj_utils.py
@@ -1,45 +1,32 @@
-#imports
 import requests
 import json
+import os
 
 from abstract_classes import GetVacanciesByAPI, VacanciesInFileJson
 
 
-class HHGetVacByAPI(GetVacanciesByAPI):
-    """Класс для получения вакансий по API"""
+class SJGetVacByAPI(GetVacanciesByAPI):
     def __init__(self, city_name, prof_name):
-        self.hh_api_url = "https://api.hh.ru"
+        self.sj_api_url = "https://api.superjob.ru/2.0"
+        self.SJ_API_TOKEN = os.getenv("SJ_TOKEN")
         self.headers = {
-            "User-Agent": "ViktorDavydov"
+            "X-Api-App-Id": self.SJ_API_TOKEN
         }
         self.city_name = city_name
         self.prof_name = prof_name
 
-    def __repr__(self):
-        return (f"Указанный город: {self.city_name}"
-                f"Указанная профессия: {self.prof_name}")
-
     def get_city_id(self):
-        """Получение id города для получения в нем вакансий"""
-        params = {
-            "text": self.city_name
-        }
-        hh_areas_url = self.hh_api_url + "/suggests/areas"
-        response = requests.get(hh_areas_url, params=params, headers=self.headers)
-
-        for city in response.json()["items"]:
-            if city["text"] == self.city_name:
-                return city["id"]
+        pass
 
     def get_vacancies(self):
         """Получение всех вакансий выбранного города"""
-        hh_vac_url = self.hh_api_url + "/vacancies"
+        sj_vac_url = self.sj_api_url + "/vacancies"
         params = {
-            "text": self.prof_name,
-            "per_page": 100,
-            "area": self.get_city_id()
+            "town": self.city_name,
+            "keyword": self.prof_name,
+            "count": 100
         }
-        response = requests.get(hh_vac_url, params=params, headers=self.headers)
+        response = requests.get(sj_vac_url, params=params, headers=self.headers)
 
         if response.status_code == 200:
             vacancies = response.json()
@@ -47,8 +34,9 @@ class HHGetVacByAPI(GetVacanciesByAPI):
         return vacancies
 
 
-class VacanciesJson(HHGetVacByAPI, VacanciesInFileJson):
+class VacanciesJson(SJGetVacByAPI, VacanciesInFileJson):
     """Класс для обработки вакансий в json формате, записи, фильтрации, сортировки и удаления"""
+
     def __init__(self, city_name, prof_name):
         super().__init__(city_name, prof_name)
 
@@ -65,45 +53,41 @@ class VacanciesJson(HHGetVacByAPI, VacanciesInFileJson):
 
     def get_all_vac_info(self):
         """Получение всего списка вакансий в формате json"""
-        return self.get_json()["items"]
+        return self.get_json()["objects"]
 
     def get_valid_vacancies_with_salary(self):
         """Получение вакансий только с указанными минимальными зарплатами"""
         valid_vacancies = []
-        for item in self.get_json()["items"]:
-            if item["salary"] is not None and item["salary"]["from"] is not None:
+        for item in self.get_json()["objects"]:
+            if item["payment_from"] != 0:
                 valid_vacancies.append(item)
         return valid_vacancies
 
     def get_vac_by_min_salary(self, salary_minimum):
         """Получение отфильтрованных вакансий по минимальной зарплате"""
         filtered_vac_by_min_sal = []
-        for item in self.get_json()["items"]:
-            if (item["salary"] is not None
-                    and item["salary"]["from"] is not None
-                    and item["salary"]["from"] >= salary_minimum):
+        for item in self.get_json()["objects"]:
+            if item["payment_from"] != 0 and item["payment_from"] >= salary_minimum:
                 filtered_vac_by_min_sal.append(item)
         return filtered_vac_by_min_sal
 
     def get_top_n_vacancies_by_sal(self, vac_count):
         """Получение топ N вакансий"""
         top_n_vac = []
-        for item in self.get_json()["items"]:
-            if item["salary"] is not None and item["salary"]["from"] is not None:
+        for item in self.get_json()["objects"]:
+            if item["payment_from"] != 0:
                 top_n_vac.append(item)
 
-        final_sorted = sorted(top_n_vac, key=lambda d: d["salary"]["from"], reverse=True)
+        final_sorted = sorted(top_n_vac, key=lambda d: d["payment_from"], reverse=True)
 
         return final_sorted[:vac_count]
 
     def get_vac_by_keyword(self, keyword):
         """Получение вакансий по ключевому слову"""
         key_word_vac = []
-        for item in self.get_json()["items"]:
-            if (item["snippet"] is not None and item["snippet"]["requirement"] is not None
-                    and item["snippet"]["responsibility"] is not None):
-                if (keyword in item["snippet"]["requirement"] or
-                        keyword in item["snippet"]["responsibility"]):
+        for item in self.get_json()["objects"]:
+            if item["candidat"] is not None:
+                if keyword in item["candidat"]:
                     key_word_vac.append(item)
         return key_word_vac
 
@@ -112,27 +96,26 @@ class VacanciesJson(HHGetVacByAPI, VacanciesInFileJson):
         vacancies_list = []
         counter = 1
         for item in input_list:
-            if item["salary"] is None:
-                salary_from = salary_to = salary_currency = "Не указано"
+            if item["payment_from"] == item["payment_to"] == 0:
+                payment_from = payment_to = currency = "Не указано"
             else:
-                if item["salary"]["from"] is None:
-                    salary_from = "Не указано"
+                if item["payment_from"] == 0:
+                    payment_from = "Не указано"
                 else:
-                    salary_from = item["salary"]["from"]
-                if item["salary"]["to"] is None:
-                    salary_to = "Не указано"
+                    payment_from = item["payment_from"]
+                if item["payment_to"] == 0:
+                    payment_to = "Не указано"
                 else:
-                    salary_to = item["salary"]["to"]
-                salary_currency = item["salary"]["currency"]
+                    payment_to = item["payment_to"]
+                currency = item["currency"]
 
             vacancies_items = f"""Вакансия {counter}
-Наименование вакансии: {item["name"]}
-Ссылка на вакансию: {item["alternate_url"]}
-Зарплата от: {salary_from}
-Зарплата до: {salary_to}\nВалюта: {salary_currency}
-Название компании: {item["employer"]["name"]}
-Требования: {item["snippet"]["requirement"]}
-Обязанности: {item["snippet"]["responsibility"]}\n"""
+Наименование вакансии: {item["profession"]}
+Ссылка на вакансию: {item["link"]}
+Зарплата от: {payment_from}
+Зарплата до: {payment_to}\nВалюта: {currency}
+Название компании: {item["client"]["title"]}
+Описание вакансии: {item["candidat"]}\n"""
 
             vacancies_list.append(vacancies_items)
             counter += 1
@@ -144,27 +127,27 @@ class VacanciesJson(HHGetVacByAPI, VacanciesInFileJson):
             pass
 
 
-class HHVacancies(VacanciesJson):
+class SJVacancies(VacanciesJson):
     """Класс для работы с вакансиями (сравнение вакансий по зарплате)"""
 
     def __init__(self, number_of_vacancy, city_name, prof_name):
         super().__init__(city_name, prof_name)
         super().get_valid_vacancies_with_salary()
         all_vacancies = self.get_valid_vacancies_with_salary()
-        self.salary_from = all_vacancies[number_of_vacancy - 1]["salary"]["from"]
+        self.salary_from = all_vacancies[number_of_vacancy - 1]["payment_from"]
 
     def __ge__(self, other):
         if isinstance(self.salary_from, int):
             return self.salary_from >= other.salary_from
 
 
-class HHUserInterface:
+class SJUserInterface:
     """Класс пользовательского интерфейса"""
 
     @staticmethod
     def user_interaction():
         """Функция работы с пользователем"""
-        print(f"Отлично! Ты выбрал платформу HeadHunter\n")
+        print(f"Отлично! Ты выбрал платформу SuperJob\n")
         city_name_input = input(f"Для начала узнаем в каком "
                                 f"городе необходимо найти вакансии, "
                                 f"например, Санкт-Петербург или Казань:\n"
@@ -173,13 +156,13 @@ class HHUserInterface:
         prof_input = input(
             f"А теперь мне необходимо узнать название профессии, например "
             f"Python разработчик или Визажист:\n> ").capitalize()
-        hh_instance = VacanciesJson(city_name_input, prof_input)
-        hh_instance.save_to_json()
+        sj_instance = VacanciesJson(city_name_input, prof_input)
+        sj_instance.save_to_json()
         print()
-        if len(hh_instance.get_all_vac_info()) == 0:
+        if len(sj_instance.get_all_vac_info()) == 0:
             print(f"Упс. Кажется не нашлось ни одной вакансии! Давай сначала!")
         else:
-            print(f"Найденное количество вакансий: {len(hh_instance.get_all_vac_info())}")
+            print(f"Найденное количество вакансий: {len(sj_instance.get_all_vac_info())}\n")
             print()
             while True:
                 print()
@@ -195,32 +178,32 @@ class HHUserInterface:
 
                 # Вывод полного списка вакансий
                 if function_input == "1":
-                    all_vacancies = hh_instance.get_all_vac_info()
-                    for item in hh_instance.formatting_output_data(all_vacancies):
+                    all_vacancies = sj_instance.get_all_vac_info()
+                    for item in sj_instance.formatting_output_data(all_vacancies):
                         print(item)
 
                 # Вывод топ N вакансий
                 elif function_input == "2":
-                    n_input = int(input(f"Введите желаемое количество вакансий для "
-                                        f"обработки:\n> "))
-                    top_n_vacancies = hh_instance.get_top_n_vacancies_by_sal(n_input)
-                    for item in hh_instance.formatting_output_data(top_n_vacancies):
+                    n_input = int(input(f"Введите желаемое количество вакансий для обработки:"
+                                        f"\n> "))
+                    top_n_vacancies = sj_instance.get_top_n_vacancies_by_sal(n_input)
+                    for item in sj_instance.formatting_output_data(top_n_vacancies):
                         print(item)
 
                 # Вывод отфильтрованных вакансий по минимальной зарплате
                 elif function_input == "3":
                     n_input = int(input(f"Введите минимальный уровень зарплаты:\n> "))
-                    vac_by_min_salary = hh_instance.get_vac_by_min_salary(n_input)
-                    for item in hh_instance.formatting_output_data(vac_by_min_salary):
+                    vac_by_min_salary = sj_instance.get_vac_by_min_salary(n_input)
+                    for item in sj_instance.formatting_output_data(vac_by_min_salary):
                         print(item)
 
                 # Вывод вакансий по ключевому слову в описании
                 elif function_input == "4":
                     keyword_input = input(f"Введите ключевое слово:\n> ")
-                    key_word_filtered_list = hh_instance.get_vac_by_keyword(keyword_input)
+                    key_word_filtered_list = sj_instance.get_vac_by_keyword(keyword_input)
                     if len(key_word_filtered_list) > 0:
 
-                        for item in hh_instance.formatting_output_data(key_word_filtered_list):
+                        for item in sj_instance.formatting_output_data(key_word_filtered_list):
                             print(item)
                     else:
                         print(f"Упс. Кажется такого слова нет ни в одном описании найденных "
@@ -228,25 +211,25 @@ class HHUserInterface:
 
                 # Очистка списка вакансий
                 elif function_input == "5":
-                    hh_instance.delete_from_json()
+                    sj_instance.delete_from_json()
                     print(f"Список вакансий очищен! Давай сначала!")
                     break
 
                 # Сравнение двух вакансий по минимальной зарплате
                 elif function_input == "6":
-                    all_vacancies = hh_instance.get_valid_vacancies_with_salary()
-                    for item in hh_instance.formatting_output_data(all_vacancies):
+                    all_vacancies = sj_instance.get_valid_vacancies_with_salary()
+                    for item in sj_instance.formatting_output_data(all_vacancies):
                         print(item)
                     print(f"Вот вакансии доступные для сравнения. В других не указана "
                           f"зарплата.\n")
                     first_vac_num_input = int(input(f"Введи порядковый номер первой "
                                                     f"вакансии:\n> "))
-                    hh_inst_1 = HHVacancies(first_vac_num_input, None, None)
+                    sj_inst_1 = SJVacancies(first_vac_num_input, None, None)
                     second_vac_num_input = int(input(f"Введи порядковый номер второй "
                                                      f"вакансии:\n> "))
-                    hh_inst_2 = HHVacancies(second_vac_num_input, None, None)
+                    sj_inst_2 = SJVacancies(second_vac_num_input, None, None)
 
-                    if hh_inst_1 >= hh_inst_2:
+                    if sj_inst_1 >= sj_inst_2:
                         print(f"Первая выбранная вакансия имеет наибольшую зарплату!")
                     else:
                         print(f"Вторая выбранная вакансия имеет наибольшую зарплату!")
